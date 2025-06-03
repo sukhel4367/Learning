3a43f94e552d993d131f9d3a0116f5b7b8c6e4f8
Sukhel Ahamed N <nsukhelahamed2003@gmail.com>
2025-06-03 10:03:41 +0530

    Updated the program with seperated class file #2

--- StudentManager1/StudentManager1/Program.cs
+++ StudentManager1/StudentManager1/Program.cs
@@ -1,68 +1,39 @@
-﻿using System.Xml.Linq;
+﻿using StudentManager1;
+using System;
+using System.Collections.Generic;
 
-public class Student
+public class Program
 {
-    public string StudentName { get; set; }
-    public Dictionary<string, int> SubjectMarks { get; set; }
-
-    public Student(string StudentName)
+    public static void Main(string[] args)
     {
-        this.StudentName = StudentName;
-        SubjectMarks = new Dictionary<string, int>();
-    }
+        List<Student> studentList = new List<Student>();
 
+        Console.WriteLine("No of students wanted to add");
+        int input = Convert.ToInt32(Console.ReadLine());
 
-    public void AddMark(string subject, int mark)
-    {
-        SubjectMarks[subject] = mark;
-    }
-    public void Print()
-    {
-        Console.WriteLine($"Student Name : {StudentName}");
-        foreach (var item in SubjectMarks)
+        for (int i = 1; i <= input; i++)
         {
-            Console.WriteLine($"{item.Key} : {item.Value}");
+            Console.Write($"Enter Student Name  :");
+            string studentName = Console.ReadLine();
+
+            Console.Write($"Enter Subject Name of Student - {i} :");
+            string subjectName = Console.ReadLine();
+
+            Console.Write($"Enter Subject Mark of Student - {i} :");
+            int subjectMark = int.Parse(Console.ReadLine());
+
+            Student student = new Student(studentName);
+            student.AddMark(subjectName, subjectMark);
+            studentList.Add(student);
         }
-    }
 
-
-
-    public class Program
-    {
-        public static void Main(string[] args)
+        Console.Write($"Type Y or y to view Student Data ");
+        string option = Console.ReadLine();
+        if (option == "Y" || option == "y")
         {
-            List<Student> studentList = new List<Student>();
-
-            Console.WriteLine("No of students wanted to add");
-            int input = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 1; i <= input; i++)
+            foreach (var student in studentList)
             {
-                Console.Write($"Enter Student Name  :");
-                string StudentName = Console.ReadLine();
-
-                Console.Write($"Enter Subject Name of Student - {i} :");
-                string SubjectName = Console.ReadLine();
-
-                Console.Write($"Enter Subject Mark of Student - {i} :");
-                int SubjectMark = int.Parse(Console.ReadLine());
-
-                Student student = new Student(StudentName);
-                student.AddMark(SubjectName, SubjectMark);
-                studentList.Add(student);
-
-
-
-            }
-            Console.Write($"Type Y or y to view Student Data ");
-            string option = Console.ReadLine();
-            if (option == "Y" || option == "y")
-            {
-                foreach (var student in studentList)
-                {
-                    student.Print();
-                }
-
+                student.Print();
             }
         }
     }
---
+++ StudentManager1/StudentManager1/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManager1
+{
+
+    public class Student
+    {
+        public string StudentName { get; set; }
+        public Dictionary<string, int> SubjectMarks { get; set; }
+
+        public Student(string StudentName)
+        {
+            this.StudentName = StudentName;
+            SubjectMarks = new Dictionary<string, int>();
+        }
+
+
+        public void AddMark(string subject, int mark)
+        {
+            SubjectMarks[subject] = mark;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"Student Name : {StudentName}");
+            foreach (var item in SubjectMarks)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+        }
+
+    }
+}
