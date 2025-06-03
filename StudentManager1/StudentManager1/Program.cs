1ac264e233a3b8e662bf0dbfe2eed617ea65ce13
Sukhel Ahamed N <nsukhelahamed2003@gmail.com>
2025-06-03 11:29:08 +0530

    Updated code for adding multiple subjects under single student name (Fixing issue #3)

--- StudentManager1/StudentManager1/Program.cs
+++ StudentManager1/StudentManager1/Program.cs
@@ -16,25 +16,37 @@ public class Program
             Console.Write($"Enter Student Name  :");
             string studentName = Console.ReadLine();
 
-            Console.Write($"Enter Subject Name of Student - {i} :");
-            string subjectName = Console.ReadLine();
+            //Console.Write($"Enter Subject Name of Student - {i} :");
+            //string subjectName = Console.ReadLine();
 
-            Console.Write($"Enter Subject Mark of Student - {i} :");
-            int subjectMark = int.Parse(Console.ReadLine());
+            //Console.Write($"Enter Subject Mark of Student - {i} :");
+            //int subjectMark = int.Parse(Console.ReadLine());
 
             Student student = new Student(studentName);
-            student.AddMark(subjectName, subjectMark);
+
+            while (true)
+            {
+                Console.Write($"Enter Subject Name for Student - {studentName} : ");
+                string subjectName = Console.ReadLine();
+
+                Console.Write($"Enter Subject Mark for {subjectName} : ");
+                int subjectMark = int.Parse(Console.ReadLine());
+
+                student.AddMark(subjectName, subjectMark);
+
+                Console.Write("Add another subject for this student? (Y/N): ");
+                string subOption = Console.ReadLine();
+                if (subOption != "Y" && subOption != "y")
+                    break;
+            }
             studentList.Add(student);
         }
 
-        Console.Write($"Type Y or y to view Student Data ");
-        string option = Console.ReadLine();
-        if (option == "Y" || option == "y")
+        
+        foreach (var student in studentList)
         {
-            foreach (var student in studentList)
-            {
-                student.Print();
-            }
+            student.Print();
         }
+        
     }
 }
